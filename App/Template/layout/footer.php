98cb9c9272f8070a9babceaca49c62c9bdd0db18
lonny <123@qq.com>
2017-05-21 13:46:41 +0800

    change

--- App/Template/layout/footer.php
+++
@@ -1,5 +0,0 @@
-	<div id="footer">
-		<span class="title">版权所有，翻版必究 <i class="fa fa-copyright"></i> 2017 | 更多请点击：<a href="javascript: void(0);" target="_blank">www.pal.com</a></span>
-	</div>
-</body>
-</html>
