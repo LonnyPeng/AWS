7e8a3c10caf788bde55767946179c8d605bad975
lonny <lonny.p@eyebuydirect.com>
2017-05-25 17:34:10 +0800

    change

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -5,11 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
         <title>@yield('title')</title>
         <link rel="icon" type="image/x-icon" href="{{ URL::asset('favicon.svg' )}}" />
-        <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/common.css' )}}" />
-        <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/font-awesome.min.css') }}" />
+        <link rel="stylesheet" type="text/css" href="{{ URL::asset('static/dist/css/common.css' )}}" />
+        <link rel="stylesheet" type="text/css" href="{{ URL::asset('static/dist/css/font.css') }}" />
         @yield('style')
-        <script type="text/javascript" src="{{ URL::asset('js/jquery-3.1.1.min.js') }}"></script>
-        <script type="text/javascript" src="{{ URL::asset('js/core.ajaxauto.js' )}}"></script>
+        <script type="text/javascript" src="{{ URL::asset('static/dist/js/jquery-3.1.1.min.js') }}"></script>
+        <script type="text/javascript" src="{{ URL::asset('static/dist/js/core.ajaxauto.js' )}}"></script>
         @yield('script')
     </head>
     <body>
